44deee7f6e6e4617e6b1b6da3cb62cd720a6af7a
tangruolin <190935423@qq.com>
2019-05-02 18:32:54 +0800

    场景1完成

--- Black/Assets/Scripts/Scene1/Leida.cs
+++ Black/Assets/Scripts/Scene1/Leida.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Leida : MonoBehaviour {
 
     private float druaTime;
     private float time = 0;
     private float colorTime;
+    private Image image;
     // Use this for initialization
     private void Start()
     {
         druaTime = Scene1.Instance.druaTime;
         colorTime = Scene1.Instance.colorTime;
+        image = GetComponent<Image>();
     }
     void OnEnable () {
         
@@ -23,7 +27,13 @@ public class Leida : MonoBehaviour {
         time += Time.deltaTime;
         if(time > druaTime)
         {
-            this.gameObject.SetActive(false);
+            image.DOFade(0, colorTime);
+            StartCoroutine(DestorySelf());
         }
 	}
+    IEnumerator DestorySelf()
+    {
+        yield return new WaitForSeconds(colorTime);
+        this.gameObject.SetActive(false);
+    }
 }
--- Black/Assets/Scripts/Scene1/Scene1.cs
+++ Black/Assets/Scripts/Scene1/Scene1.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class Scene1 : MonoBehaviour {
 
 
-    public float druaTime = 1;//雷达边消失时间
+    public float druaTime;//雷达边消失时间
     public float intervalTime;//雷达边出现间隔时间
-    public float colorTime = 0.5f;//雷达颜色渐变时间
+    public float colorTime ;//雷达颜色渐变时间
     public Transform leidaBack;
     public Transform leidaFront;
-    private List<GameObject> back = new List<GameObject>();
-    private List<GameObject> front = new List<GameObject>();
+    private List<Image> back = new List<Image>();
+    private List<Image> front = new List<Image>();
     private bool isEnd = true;
 
-    private float time = 0;
     public static Scene1 Instance;
     private bool isShow = false;
+    private float time = 0;
+    public GameObject santi;
 
     private void Awake()
     {
@@ -25,17 +28,22 @@ public class Scene1 : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
 		for(int i = 0; i < leidaBack.childCount; i++)
         {
-            back.Add(leidaBack.GetChild(i).gameObject);
-            front.Add(leidaFront.GetChild(i).gameObject);
+            back.Add(leidaBack.GetChild(i).GetComponent<Image>());
+            front.Add(leidaFront.GetChild(i).GetComponent<Image>());
         }
         StartCoroutine(Show());
     }
 	
 	// Update is called once per frame
 	void Update () {
-      
+        //time += Time.deltaTime;
+        //if(time > 10)
+        //{
+            
+        //}
 	}
 
     public void OnButtonDown()
@@ -58,8 +66,14 @@ public class Scene1 : MonoBehaviour {
                 for (int i = 0; i < back.Count; i++)
                 {
                     yield return new WaitForSeconds(intervalTime);
-                    back[i].SetActive(true);
-                    front[i].SetActive(true);
+                    back[i].gameObject.SetActive(true);
+                    back[i].DOFade(1,colorTime);
+                    front[i].gameObject.SetActive(true);
+                    front[i].DOFade(1, colorTime);
+                    if(!santi.activeSelf && i == 7)
+                    {
+                        StartCoroutine(Santi());
+                    }
                     if (!isShow)
                     {
                         break;
@@ -73,11 +87,17 @@ public class Scene1 : MonoBehaviour {
         
     }
 
+    IEnumerator Santi()
+    {
+        yield return new WaitForSeconds(colorTime);
+        santi.SetActive(true);
+    }
+
     bool IsEnd()
     {
         for(int i = 0; i < back.Count; i++)
         {
-            if (back[i].activeSelf)
+            if (back[i].color.a == 1)
             {
                 return false;
             }

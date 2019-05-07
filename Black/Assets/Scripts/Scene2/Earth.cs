e9e1538dec0d63e25fc69728fd63705888a8dec1
tangruolin <190935423@qq.com>
2019-05-07 21:04:28 +0800

    scene2

---
+++ Black/Assets/Scripts/Scene2/Earth.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Earth : MonoBehaviour {
+
+    public float time = 3;
+    public static Earth Instance;
+    public GameObject Santi;
+    private List<Vector3> pos = new List<Vector3>();
+    private Transform oldPos;
+    [HideInInspector]
+    public int num = 0;
+    private bool canTouch = true;
+    private List<SantiStar> starList = new List<SantiStar>();
+    private System.Random random = new System.Random();
+    private bool destory = false;
+    private int pointNum = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        oldPos = GameObject.Find("oldPos").transform;
+        for(int i = 0; i < oldPos.childCount; i++)
+        {
+            pos.Add(oldPos.GetChild(i).position);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (canTouch)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                pointNum++;
+                Create();
+            }
+        }
+        if(pointNum == 10)
+        {
+            canTouch = false;
+        }
+        if(num == 10)
+        {
+            StartCoroutine(SetPos());
+            num = 0;
+            pointNum = 0;
+
+        }
+
+    }
+   private void Create()
+    {
+        SantiStar san = null;
+        for(int i = 0; i < starList.Count; i++)
+        {
+            if (!starList[i].go.activeSelf)
+            {
+                san = starList[i];
+            }
+        }
+        if (san == null)
+        {
+            san = new SantiStar(Santi);
+            starList.Add(san);
+        }
+        san.SetData(pos[random.Next(0, pos.Count)]);
+        san.ReStart();
+    }
+    private void Clear()
+    {
+
+    }
+    IEnumerator SetPos()
+    {
+
+        for (int i = 0; i < starList.Count; i++)
+        {
+            starList[i].Destory(time);
+        }
+        yield return new WaitForSeconds(time+1);
+        for (int i = 0; i < starList.Count; i++)
+        {
+            starList[i].go.SetActive(false);
+            starList[i].go.transform.SetParent(oldPos);
+        }
+        canTouch = true;
+    }
+}
+
+public class SantiStar
+{
+    public GameObject go;
+    public SantiStar(GameObject model) {
+        go = GameObject.Instantiate(model);
+    }
+
+    public void SetData(Vector3 pos)
+    {
+        go.transform.position = pos;
+    }
+
+    public void Destory(float time)
+    {
+        go.GetComponent<MeshRenderer>().material.DOFade(0,time);
+    }
+    public void ReStart()
+    {
+        go.GetComponent<MeshRenderer>().material.DOFade(1,0.01f);
+        go.SetActive(true);
+        go.GetComponent<Star>().move = true;
+    }
+}
---
+++ Black/Assets/Scripts/Scene2/Star.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Star : MonoBehaviour {
+
+    public float speed = 0.1f;
+    public Transform earth1;
+    public Transform earth;
+    [HideInInspector]
+    public bool move = false;
+    private Vector3 oldpos;
+    private Transform santi;
+    private float time;
+	// Use this for initialization
+	void Start () {
+        //transform.LookAt(Earth);
+        oldpos = transform.position;
+        santi = transform.Find("oldPos");
+        time = Earth.Instance.time;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //if (Vector3.Distance(transform.position, earth.position) <= 1)
+        //{
+        //    move = false;
+        //    transform.SetParent(earth);
+        //}
+
+        if (move)
+        {
+            float dis = Vector3.Distance(transform.position, earth1.position) - 1f;
+            if ( dis <speed)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, earth1.position, dis);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, earth1.position, speed);
+            }
+          //  Debug.Log(Vector3.Distance(transform.position, earth1.position));
+            
+
+        }
+        // transform.Translate(transform.forward*speed);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball" || other.gameObject.tag == "Earth")
+        {
+            move = false;
+            transform.SetParent(earth);
+            Earth.Instance.num++;
+        }
+    }
+   
+    //IEnumerator SetPos()
+    //{
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(time);
+    //        transform.position = oldpos;
+    //        transform.SetParent(santi);
+    //        move = true;
+    //    }
+        
+    //}
+   
+}

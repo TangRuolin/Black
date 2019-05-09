03d8865d3e8fc8f836e4e35d853adfbf83a0bd94
tangruolin <190935423@qq.com>
2019-05-09 16:49:24 +0800

    Scene5 完成

--- Black/Assets/Scripts/Scene5/Touch5.cs
+++ Black/Assets/Scripts/Scene5/Touch5.cs
@@ -2,17 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Touch : MonoBehaviour {
+public class Touch5 : MonoBehaviour {
 
+    public float factor = 1000f;
 	// Use this for initialization
 	void Start () {
 		
 	}
     private UnityEngine.Touch oldTouch1;  //上次触摸点1(手指1)
     private UnityEngine.Touch oldTouch2;  //上次触摸点2(手指2)
-                              // Update is called once per frame
-    void Update () {
-        if (Input.touchCount <= 0)
+    bool move = false;
+    Vector2 oldPosition;
+    Vector2 newPosition;// Update is called once per frame
+    void Update()
+    {
+
+#if UNITY_EDITOR
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            oldPosition = Input.mousePosition;
+            move = true;
+
+        }
+        if (move)
+        {
+            newPosition = Input.mousePosition;
+            float dis = Vector2.Distance(oldPosition, newPosition);
+            float scaleFactor = dis / factor;
+            if (scaleFactor > 0.005f)
+            {
+                scaleFactor = 0.005f;
+            }
+            if (transform.localScale.y <= 0.02f)
+            {
+                if (transform.localScale.x > 0)
+                    transform.localScale -= new Vector3(scaleFactor, 0, 0);
+                if (transform.localScale.x < 0)
+                {
+                    transform.localScale = new Vector3(0, 0, 0);
+                }
+            }
+            else
+            {
+                transform.localScale -= new Vector3(0, scaleFactor, 0);
+
+            }
+            oldPosition = newPosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            move = false;
+        }
+#elif UNITY_IPHONE || UNITY_ANDROID
+
+      
+          if (Input.touchCount <= 0)
         {
             return;
         }
@@ -36,18 +81,29 @@ public class Touch : MonoBehaviour {
             offset = 0;
         }
         //放大因子， 一个像素按 0.01倍来算(100可调整)
-        float scaleFactor = offset / 100f;
-        Vector3 localScale = transform.localScale;
-        Vector3 scale = new Vector3(localScale.x + scaleFactor,
-            localScale.y + scaleFactor,
-            localScale.z + scaleFactor);
-        //在什么情况下进行缩放
-        if (scale.x >= 0.05f && scale.y >= 0.05f && scale.z >= 0.05f)
-        {
-            transform.localScale = scale;
-        }
+        float scaleFactor = offset / factor;
+         if(scaleFactor > 0.005f)
+            {
+                scaleFactor = 0.005f;
+            }
+          if (transform.localScale.y <= 0.01f)
+            {
+                if(transform.localScale.x >0)
+                   transform.localScale += new Vector3(scaleFactor, 0, 0);
+                if(transform.localScale.x < 0)
+                {
+                    transform.localScale = new Vector3(0, 0, 0);
+                }
+            }
+            else
+            {
+                transform.localScale += new Vector3(0, scaleFactor, 0);
+
+            }
+        
         //记住最新的触摸点，下次使用
         oldTouch1 = newTouch1;
         oldTouch2 = newTouch2;
+#endif
     }
 }
